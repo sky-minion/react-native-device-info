43b863f532605b34187ee76106edc35123630952
Jagdeep Nagpal <jagdeepnagpal@gmail.com>
2016-08-22 10:06:04 +0100

    Update RNDeviceModule.java

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -75,7 +75,9 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
 
     try {
       BluetoothAdapter myDevice = BluetoothAdapter.getDefaultAdapter();
-      deviceName = myDevice.getName();
+      if (myDevice != null) {
+        deviceName = myDevice.getName();
+      }
     } catch(Exception e) {
       e.printStackTrace();
     }
